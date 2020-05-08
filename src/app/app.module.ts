e2eb7add473bfd1e655b0d7c6277da5a359849fd
mviitala <mikko.k.viitala@trimble.com>
2020-05-08 14:09:23 +0300

    refactor-material-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,15 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {LoginComponent} from './user/login/login.component';
 import {AmplifyAngularModule, AmplifyService} from 'aws-amplify-angular';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {
-  ErrorStateMatcher,
-  MatButtonModule,
-  MatCardModule, MatCheckboxModule,
-  MatDialogModule,
-  MatDividerModule,
-  MatFormFieldModule,
-  MatGridListModule,
-  MatIconModule,
-  MatInputModule,
-  MatMenuModule,
-  MatProgressSpinnerModule,
-  MatRadioModule,
-  MatRippleModule,
-  MatSnackBarModule,
-  ShowOnDirtyErrorStateMatcher
-} from '@angular/material';
+import {ErrorStateMatcher, ShowOnDirtyErrorStateMatcher} from '@angular/material';
 import {FlexLayoutModule, FlexModule} from '@angular/flex-layout';
 import {TranslateModule} from '@ngx-translate/core';
-import {translateConfig} from '../assets/translate-config';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-
 import {SnackbarService} from './service/snackbar.service';
 import {AuthService} from './service/auth.service';
-
 import {MemoListComponent} from './memo/memo-list/memo-list.component';
 import {RegistrationComponent} from './user/registration/registration.component';
 import {UnauthorizedInterceptor} from './service/interceptor/unauthorized.interceptor';
@@ -39,11 +17,9 @@ import {Router} from '@angular/router';
 import {ConfirmRegistrationComponent} from './user/confirm-registration/confirm-registration.component';
 import {ForgotPasswordComponent} from './user/forgot-password/forgot-password.component';
 import {ResetPasswordComponent} from './user/reset-password/reset-password.component';
-
 import {MemoListItemComponent} from './memo/memo-list-item/memo-list-item.component';
 import {LocalDatePipe} from './pipe/local-date.pipe';
 import {DatePipe, registerLocaleData} from '@angular/common';
-import localeFi from '@angular/common/locales/fi';
 import {MemoEditorComponent} from './memo/dialog/memo-editor/memo-editor.component';
 import {CardLayoutComponent} from './card-layout/card-layout.component';
 import {LanguageSelectorComponent} from './header/language-selector/language-selector.component';
@@ -51,6 +27,11 @@ import {MenuComponent} from './header/menu/menu.component';
 import {HeaderComponent} from './header/header.component';
 import {FooterComponent} from './footer/footer.component';
 import {ConfirmDeleteMemoComponent} from './memo/dialog/confirm-delete-memo/confirm-delete-memo.component';
+import {MaterialComponentsModule} from './material-components.module';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {translateConfig} from '../assets/translate-config';
+import localeFi from '@angular/common/locales/fi';
 
 registerLocaleData(localeFi, 'fi');
 
@@ -78,27 +59,13 @@ registerLocaleData(localeFi, 'fi');
     AppRoutingModule,
     AmplifyAngularModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatCardModule,
     FlexModule,
     HttpClientModule,
     TranslateModule.forRoot(translateConfig),
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
     ReactiveFormsModule,
-    MatDialogModule,
-    MatProgressSpinnerModule,
-    MatSnackBarModule,
-    MatMenuModule,
-    MatIconModule,
     FlexLayoutModule,
-    MatRadioModule,
     FormsModule,
-    MatGridListModule,
-    MatRippleModule,
-    MatDividerModule,
-    MatCheckboxModule
+    MaterialComponentsModule
   ],
   providers: [
     DatePipe,
