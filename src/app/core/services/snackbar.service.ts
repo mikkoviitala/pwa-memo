8e8d8c23f702c37db87acc6556eca56ec892b0c3
mviitala <mikko.k.viitala@trimble.com>
2020-05-12 14:00:45 +0300

    localize undo action

--- src/app/core/services/snackbar.service.ts
+++ src/app/core/services/snackbar.service.ts
@@ -8,25 +8,39 @@ import {SimpleSnackBar} from '@angular/material/snack-bar/typings/simple-snack-b
   providedIn: 'root'
 })
 export class SnackbarService {
-  private readonly showDuration: number = 1800;
-  private readonly showUndoDuration: number = 3000;
+  private readonly duration: number = 1800;
+  private readonly undoDuration: number = 3000;
 
-  constructor(private snackbar: MatSnackBar, private translate: TranslateService) {
+  constructor(
+    private snackbar: MatSnackBar,
+    private translate: TranslateService) {
   }
 
   show(message: string, params?: any): void {
-    this._open(message, null, params || null, this.showDuration);
+    this._open(
+      message,
+      params || null,
+      null,
+      this.duration);
   }
 
   showUndo(message: string, params?: any): MatSnackBarRef<SimpleSnackBar> {
-    return this._open(message, 'Undo', params || null, this.showUndoDuration);
+    return this._open(
+      message,
+      params || null,
+      'common.undo',
+      this.undoDuration);
   }
 
-  private _open(message: string, action: string, params: any, duration: number): MatSnackBarRef<SimpleSnackBar> {
+  private _open(message: string, params: any, action: string, duration: number): MatSnackBarRef<SimpleSnackBar> {
     const content = params ? this.translate.instant(message, params) : this.translate.instant(message);
-    return this.snackbar.open(content, action, {
-      duration,
-      panelClass: 'snackbar' /* ['mat-toolbar', 'mat-primary'] */
-    });
+    const buttonText = action ? this.translate.instant(action) : null;
+
+    return this.snackbar.open(
+      content,
+      buttonText, {
+        duration,
+        panelClass: 'snackbar' /* ['mat-toolbar', 'mat-primary'] */
+      });
   }
 }
